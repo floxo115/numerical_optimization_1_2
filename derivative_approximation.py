2e9dd022d0f2d21d27d80f0b85c85cca39a86d6f
floxo115 <flojak@gmx.net>
2023-05-08 15:02:14 +0200

    worked on approx. derivative with cdf

---
+++ derivative_approximation.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+# estimate gradient with central difference formula (Nocedal 196)
+def cdf_grad(f, x, epsilon=10 ** -6):
+    size = x.size
+    gradient = np.zeros(size, dtype=np.float128)
+
+    for i in range(size):
+        e = np.zeros(size)
+        e[i] = 1
+        gradient[i] = (f(x + epsilon * e) - f(x - epsilon * e)) / (2 * epsilon)
+
+    return gradient
+
+
+# estimate hessian empirically (Nocedal 202)
+def cdf_hessian(f, x, epsilon=10 ** -7):
+    size = x.size
+
+    hessian = np.zeros((size, size), dtype=np.float128)
+
+    for row in range(size):
+        for col in range(size):
+            e_i = np.zeros(size, dtype=np.float128)
+            e_j = np.zeros(size, dtype=np.float128)
+            e_i[row] = 1
+            e_j[col] = 1
+
+            hessian[row, col] = (f(x + epsilon * e_i + epsilon * e_j) -
+                                 f(x + epsilon * e_i) -
+                                 f(x + epsilon * e_j) + f(x)) / epsilon ** 2
+
+    # hessian = (hessian + hessian.T) / 2
+    return hessian
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import numpy as np
-import utils
+import torch.func
 
+import utils
+from derivative_approximation import cdf_grad
 import test_functions
 
 print(test_functions.rosenbrock_func.get_gradient(np.array([1., 1.])))
 plot = utils.plot_2d_contour(np.linspace(0, 1, 100), np.linspace(0, 1, 100),
                              test_functions.test_func_2.get_function())
 plot.show()
+
+print(cdf_grad(lambda x: x ** 2, np.array([8]), 1))
+print(test_functions.TestFunctionTorch(lambda x: x[0]**2 + x[1]**2).get_gradient(np.array([8.,8.])))
