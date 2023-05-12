dcfbe704a2f4e15e60f2998c0a42acb764f827b1
floxo115 <flojak@gmx.net>
2023-05-12 16:08:09 +0200

    first test of newton method with HM

--- main.py
+++ main.py
@@ -3,12 +3,66 @@ import torch.func
 
 import utils
 from derivative_approximation import cdf_grad
-import test_functions
+from test_functions import test_func_1, test_func_2, START_POINTS
+from newton_method_mod import NewtonMethodWithModifiedHessian
+from utils import plot_2d_contour
+from matplotlib import pyplot as plt
 
-print(test_functions.rosenbrock_func.get_gradient(np.array([1., 1.])))
-plot = utils.plot_2d_contour(np.linspace(0, 1, 100), np.linspace(0, 1, 100),
-                             test_functions.test_func_2.get_function())
-plot.show()
+# print(test_functions.rosenbrock_func.get_gradient(np.array([1., 1.])))
+# plot = utils.plot_2d_contour(np.linspace(0, 1, 100), np.linspace(0, 1, 100),
+#                              test_functions.test_func_2.get_function())
+# plot.show()
+#
+# print(cdf_grad(lambda x: x ** 2, np.array([8]), 1))
+# print(test_functions.TestFunctionTorch(lambda x: x[0]**2 + x[1]**2).get_gradient(np.array([8.,8.])))
 
-print(cdf_grad(lambda x: x ** 2, np.array([8]), 1))
-print(test_functions.TestFunctionTorch(lambda x: x[0]**2 + x[1]**2).get_gradient(np.array([8.,8.])))
+f = test_func_1
+nm = NewtonMethodWithModifiedHessian(f)
+for start_point in START_POINTS["test_function_1"]:
+    print("----------------------------------------")
+    print("optimizing with newton method with hessian modifications")
+    print(f"test function 1; starting point ({start_point[0], start_point[1]})")
+
+    xs = nm.run_optim(start_point)
+
+    print(f"needed {len(xs)} iterations")
+
+    x_lims = [-4,4]
+    y_lims = [-4,4]
+
+    plot_2d_contour(
+        np.linspace(x_lims[0], x_lims[1], 100),
+        np.linspace(x_lims[0], x_lims[1], 100),
+        test_func_1.function
+    )
+
+    plt.scatter([x[0] for x in xs], [x[1] for x in xs])
+    plt.show()
+
+    print(f"solution found at ({xs[-1][0]}, {xs[-1][1]})")
+    print(f"solution value {f.function(xs[-1])}")
+
+f = test_func_2
+nm = NewtonMethodWithModifiedHessian(f)
+for start_point in START_POINTS["test_function_2"]:
+    print("----------------------------------------")
+    print("optimizing with newton method with hessian modifications")
+    print(f"test function 2; starting point ({start_point[0], start_point[1]})")
+
+    xs = nm.run_optim(start_point)
+
+    print(f"needed {len(xs)} iterations")
+
+    x_lims = [-6,6]
+    y_lims = [-6,6]
+
+    plot_2d_contour(
+        np.linspace(x_lims[0], x_lims[1], 100),
+        np.linspace(x_lims[0], x_lims[1], 100),
+        test_func_2.function
+    )
+
+    plt.scatter([x[0] for x in xs], [x[1] for x in xs])
+    plt.show()
+    print(f"solution found at ({xs[-1][0]}, {xs[-1][1]})")
+    print(f"solution value {f.function(xs[-1])}")
