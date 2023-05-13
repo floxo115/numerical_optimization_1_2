ba0216a92a96d3d30aa673f72c4379497143259c
floxo115 <flojak@gmx.net>
2023-05-13 09:23:23 +0200

    cleaned up main

--- main.py
+++ main.py
@@ -5,53 +5,39 @@ from newton_method_mod import NewtonMethodWithModifiedHessian
 from utils import plot_2d_contour
 from matplotlib import pyplot as plt
 
-f = test_func_1
-nm = NewtonMethodWithModifiedHessian(f)
-for start_point in START_POINTS["test_function_1"]:
-    print("----------------------------------------")
-    print("optimizing with newton method with hessian modifications")
-    print(f"test function 1; starting point ({start_point[0], start_point[1]})")
 
-    xs = nm.run_optim(start_point)
-
-    print(f"needed {len(xs)} iterations")
-
-    x_lims = [-4, 4]
-    y_lims = [-4, 4]
-
+def plot_test_run(f, xs, x_lims, y_lims):
     plot_2d_contour(
         np.linspace(x_lims[0], x_lims[1], 100),
         np.linspace(x_lims[0], x_lims[1], 100),
-        test_func_1.function
+        f.function
     )
 
     plt.scatter([x[0] for x in xs], [x[1] for x in xs], c=[i for i in range(len(xs))], cmap="plasma")
     plt.show()
 
-    print(f"solution found at ({xs[-1][0]}, {xs[-1][1]})")
-    print(f"solution value {f.function(xs[-1])}")
+
+def run_test(description, f, start_points, optim, plot=False):
+    for start_point in start_points:
+        print("----------------------------------------")
+        print("optimizing with newton method with hessian modifications")
+        print(f"test function 1; starting point ({start_point[0], start_point[1]})")
+
+        xs = optim.run_optim(start_point)
+
+        print(f"needed {len(xs)} iterations")
+
+        print(f"solution found at ({xs[-1][0]:.5}, {xs[-1][1]:.5})")
+        print(f"solution value {f.function(xs[-1]):.5}")
+
+        if plot:
+            plot_test_run(f, xs, [-6, 6], [-6, 6])
+
+
+f = test_func_1
+nm = NewtonMethodWithModifiedHessian(f)
+run_test("NM with Hessian Modificatoin on Rosenbrock function", f, START_POINTS["test_function_1"], nm, plot=True)
 
 f = test_func_2
 nm = NewtonMethodWithModifiedHessian(f)
-for start_point in START_POINTS["test_function_2"]:
-    print("----------------------------------------")
-    print("optimizing with newton method with hessian modifications")
-    print(f"test function 2; starting point ({start_point[0], start_point[1]})")
-
-    xs = nm.run_optim(start_point)
-
-    print(f"needed {len(xs)} iterations")
-
-    x_lims = [-6, 6]
-    y_lims = [-6, 6]
-
-    plot_2d_contour(
-        np.linspace(x_lims[0], x_lims[1], 100),
-        np.linspace(x_lims[0], x_lims[1], 100),
-        test_func_2.function
-    )
-
-    plt.scatter([x[0] for x in xs], [x[1] for x in xs], c=[i for i in range(len(xs))], cmap="plasma")
-    plt.show()
-    print(f"solution found at ({xs[-1][0]}, {xs[-1][1]})")
-    print(f"solution value {f.function(xs[-1])}")
+run_test("NM with Hessian Modificatoin on test function 2", f, START_POINTS["test_function_2"], nm, plot=True)
