53e4a251c9802593392a7927a696d9fc2cd953f8
floxo115 <flojak@gmx.net>
2023-05-17 10:59:47 +0200

    worked on conjugate gradient with FR

---
+++ conjugate_gradient.py
@@ -0,0 +1,49 @@
+import line_search
+from test_functions import TestFunction
+from utils import MaxIterationsError
+import numpy as np
+
+
+class ConjugateGradientFR():
+    def __init__(self, f: TestFunction, alpha: float):
+        self.f = f
+        self.alpha = alpha
+        self.last_nabla_x = None
+        self.last_d = None
+
+    def step(self, x):
+        nabla_x = self.f.get_gradient(x)
+
+        if self.last_d is not None:
+            beta = np.dot(nabla_x, nabla_x)/ np.dot(self.last_nabla_x, self.last_nabla_x)
+            d = -nabla_x + beta * self.last_d
+        else:
+            d = -nabla_x
+
+        self.last_nabla_x = nabla_x
+        self.last_d = d
+
+        alpha = line_search.backtracking_line_search(self.f, x, d, self.alpha)
+        return x + alpha * d
+
+    def run_optim(self, start: np.ndarray, stop_grad=10 ** -6, max_iterations=1000):
+        k = 0
+        x = start
+        xs = [x]
+        while True:
+            if np.linalg.norm(self.f.get_gradient(x)) < stop_grad:
+                break
+            if k > max_iterations:
+                raise MaxIterationsError()
+
+            x = self.step(x)
+            xs.append(x)
+
+            k += 1
+
+        xs.append(x)
+        return xs
+
+    def reset(self):
+        self.last_d = None
+        self.last_nabla_x = None
